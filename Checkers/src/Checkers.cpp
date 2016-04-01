855f100bd0e4703540af55e2bd52b000ec75c19b
carlosA-A <carlosA-A@users.noreply.github.com>
2016-04-01 16:09:51 -0400

    initial commit
    Array that prints board

---
+++ Checkers/src/Checkers.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : Checkers.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int board[8][8];
+	for (int spot = 0;  spot < 8; ++ spot) {
+			for (int cell = 0; cell < 8; ++ cell) {
+				board[spot][cell] = 0;
+			}
+
+		}
+
+	for (int spot = 0;  spot < 8; ++ spot) {
+		for (int cell = 0; cell < 8; ++ cell) {
+			cout <<  board[spot][cell]<<'\t';
+			if (cell ==7) {
+
+				cout<<endl;
+			}
+		}
+		cout<<endl;
+
+
+	}
+	return 0;
+}
