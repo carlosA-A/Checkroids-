e2fc16135380bcd103e0cb31dc81bc70ac6c7d2a
RB <c.avogadro@ufl.edu>
2016-04-16 14:02:35 -0400

    Ready to merge

--- Checkers/src/Checkers.cpp
+++ Checkers/src/Checkers.cpp
@@ -13,9 +13,7 @@ using namespace std;
 
 int main() {
 	
-	Piece* pieceArray[32];	
-	cout<<pieceArray[0];
-
+	
 	int board[8][8];
 	for (int spot = 0;  spot < 8; ++ spot) {
 			for (int cell = 0; cell < 8; ++ cell) {
