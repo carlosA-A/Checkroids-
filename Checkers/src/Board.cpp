25ba1f29badaff59a00e38da3f138cc2905c6ec9
Sebastian <sperez5553@gmail.com>
2016-04-16 18:53:21 -0400

    Board

---
+++ Checkers/src/Board.cpp
@@ -0,0 +1,30 @@
+#include "Pieces.cpp"
+
+void Board::setUpBoard(){
+	
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			
+		}
+	}
+	
+	/* for(int index = 0; index < 24; index++){
+		if(index < 8){
+			if((index%2) == 1){
+				pieceArray[index] = new Normal(index, 0, true);
+			}
+		}
+		
+		else if(index < 16){
+			if((index%2) == 0){
+				pieceArray[index] = new Normal(index, 1, true);
+			}
+		}
+		
+		else if(index < 24){
+			if((index%2) == 1){
+				pieceArray[index] = new Normal(index, 2, true);
+			}
+		}
+	} */
+}
