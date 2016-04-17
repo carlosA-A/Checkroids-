21de14554b55aae94e70ed8bbfaee13c77cc8b4b
RB <c.avogadro@ufl.edu>
2016-04-17 09:45:45 -0400

    Added Board files

---
+++ Checkers/src/Board.h
@@ -0,0 +1,49 @@
+#ifndef BOARD_H_
+#define BOARD_H_
+#include "Pieces.h"
+
+
+
+class Board{
+
+Pieces* array[8][8];
+
+void populateBoard();
+void printBoard();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* PIECES_H_ */
