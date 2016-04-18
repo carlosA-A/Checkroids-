96a5788a0f60ce6db0315de782883a8e062c65e2
lflood <lflood@ufl.edu>
2016-04-18 18:09:15 -0400

    Update Pieces.h
    spacing and comments

--- Checkers/src/Pieces.h
+++ Checkers/src/Pieces.h
@@ -11,57 +11,54 @@ int whitePiecesLeft = 12;
 int blackPiecesLeft = 12;
 
 class Piece{
-
 	public:
-		friend class Board;
+	
+	
+	friend class Board;
 
 
-		int move[2];  		//Where the piece is trying to move
-		int position[2];					// x and y coordinates of piece are stored in array
-		int destination[2];
-		bool isWhite;					//black or white piece
-		bool dead;					//still in play on board
-		int blackScore;
-		int whiteScore;
-		int countDeadPieces;
-		bool isLegal;
-		bool firstMoved;
-		bool didMove;  //Checks is the piece has already moved
-		bool exists;
-		bool didJump;
-		bool pieceCanJump;
-		int type;	//Type 0 is normal, type 1 is King
+	int move[2];  		//Where the piece is trying to move
+	int position[2];	// x and y coordinates of piece are stored in array
+	int destination[2];
+	bool isWhite;		//black or white piece
+	bool dead;		//still in play on board
+	int blackScore;
+	int whiteScore;
+	int countDeadPieces;
+	bool isLegal;
+	bool firstMoved;
+	bool didMove;  		//Checks if the piece has already moved
+	bool exists;
+	bool didJump;
+	bool pieceCanJump;
+	int type;		//Type 0 is normal, type 1 is King
 
-		Piece(){};
-		Piece(bool exists,int positionX,int positionY, bool isWhite, int type)
-		{
-			this -> position[0] = positionX;
-			this -> position[1] = positionY;
-			this -> isWhite = isWhite;
-			this -> exists = exists;
-			this -> type = type;
-
-
-		}
-		int* getPosition();
-		void setPosition(int x, int y);
-		int getX();
-		int getY();
-		bool getWhite();		//Returns true if piece is white else it's black
-		void setWhite(bool isWhite);
-		int getDead();		//Returns the number of dead pieces
-	 	void setDead();		//Returns true if piece has been destroyed
-		virtual bool isMoveLegal(int x, int y, int currentX, int currentY){return isLegal;};  //checks where piece is and wants to go, if move is legal return true
-
-		bool getDidMove();
+	Piece(){};
+	Piece(bool exists,int positionX,int positionY, bool isWhite, int type)
+	{
+		this -> position[0] = positionX;
+		this -> position[1] = positionY;
+		this -> isWhite = isWhite;
+		this -> exists = exists;
+		this -> type = type;
 
 
+	}
+	int* getPosition();
+	void setPosition(int x, int y);
+	int getX();
+	int getY();
+	bool getWhite();	//Returns true if piece is white else it's black
+	void setWhite(bool isWhite);
+	int getDead();		//Returns the number of dead pieces
+ 	void setDead();		//Returns true if piece has been destroyed
+	virtual bool isMoveLegal(int x, int y, int currentX, int currentY){return isLegal;};  //checks where piece is and wants to go, if move is legal return true
+
+	bool getDidMove();
 };
+
 class Normal: public Piece{
-
-public:
-
-
+	public:
 
 
 	Normal(bool exists,int positionX, int positionY, bool isWhite,int type):Piece(exists,positionX,positionY,isWhite,type)
@@ -74,13 +71,13 @@ public:
 class King: public Piece{
 	public:
 
+
 	King(bool exists,int positionX, int positionY, bool isWhite,int type):Piece(exists,positionX,positionY,isWhite,type)
 	{
 
 	}
 
 	bool isMoveLegal(int x, int y, int currentX, int currentY);
-
 };
 
 #endif /* PIECES_H_ */
